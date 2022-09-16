d2a3d1461683d42771556582147a02b914d7bc5b
syoma <skat.apps@gmail.com>
2022-09-16 13:10:13 +0300

    add return Call

--- app/src/main/java/com/example/guapstudios/data/retrofitService/AuthorizationRetrofitService.kt
+++ app/src/main/java/com/example/guapstudios/data/retrofitService/AuthorizationRetrofitService.kt
@@ -1,11 +1,14 @@
 package com.example.guapstudios.data.retrofitService
 
 import com.example.guapstudios.data.emptities.LoginReciveModel
+import com.example.guapstudios.data.emptities.TokenModel
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthorizationRetrofitService {
     @POST("login")
-    fun login(@Body loginReciveModel: LoginReciveModel)
+    fun login(@Body loginReciveModel: LoginReciveModel): Call<TokenModel>
+
 }
