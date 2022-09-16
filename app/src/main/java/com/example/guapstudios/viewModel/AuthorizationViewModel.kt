d3369c7c56b4a3bb7571a7ae4422b92462301775
syoma <skat.apps@gmail.com>
2022-09-16 20:09:42 +0300

    realization authorization in viewModel

--- app/src/main/java/com/example/guapstudios/viewModel/AuthorizationViewModel.kt
+++ app/src/main/java/com/example/guapstudios/viewModel/AuthorizationViewModel.kt
@@ -6,26 +6,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.guapstudios.data.RetrofitClient
 import com.example.guapstudios.data.emptities.LoginReciveModel
+import com.example.guapstudios.data.emptities.RegisterReciveModel
 import com.example.guapstudios.data.emptities.TokenModel
+import com.example.guapstudios.data.emptities.User
 import com.example.guapstudios.data.retrofitService.AuthorizationRetrofitService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
 
 class AuthorizationViewModel : ViewModel() {
+
+    private val client = RetrofitClient.getAuthorizationRetrofitService()
+        .create(AuthorizationRetrofitService::class.java)
 
     private val _isAuthorization = MutableLiveData<Boolean>(false)
     val isAuthorization: LiveData<Boolean>
         get() = _isAuthorization
 
     fun login(reciveModel: LoginReciveModel) {
-        val client = RetrofitClient.getAuthorizationRetrofitService()
-            .create(AuthorizationRetrofitService::class.java)
-
         client.login(reciveModel).enqueue(object : Callback<TokenModel> {
             override fun onResponse(call: Call<TokenModel>, response: Response<TokenModel>) {
                 if (response.isSuccessful) {
-                    _isAuthorization.value = true
+                    response.body()?.let { getUserByToken(it) }
                 } else {
                     Log.i("Retrofit", "login viewMOdel error: ${response.errorBody()}")
                 }
@@ -33,6 +36,39 @@ class AuthorizationViewModel : ViewModel() {
 
             override fun onFailure(call: Call<TokenModel>, t: Throwable) {
                 Log.i("Retrofit", "login view Model: $t")
+            }
+
+        })
+    }
+
+    fun register(registerReciveModel: RegisterReciveModel) {
+        client.register(registerReciveModel).enqueue(object : Callback<TokenModel> {
+            override fun onResponse(call: Call<TokenModel>, response: Response<TokenModel>) {
+                if (response.isSuccessful) {
+                    response.body()?.let { getUserByToken(it) }
+                } else {
+                    Log.i("Retrofit", "login viewMOdel error: ${response.errorBody()}")
+                }
+            }
+
+            override fun onFailure(call: Call<TokenModel>, t: Throwable) {
+                Log.i("Retrofit", "register viewModel: $t")
+            }
+        })
+    }
+
+    fun getUserByToken(tokenModel: TokenModel) {
+        client.getUserByToken(tokenModel).enqueue( object  : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    _isAuthorization.value = true
+                } else {
+                    Log.i("Retrofit", "login viewMOdel error: ${response.errorBody()}")
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Log.i("Retrofit", "token viewModel: $t")
             }
 
         })
