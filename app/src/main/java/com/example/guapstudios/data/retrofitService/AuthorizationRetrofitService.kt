4a8033273cd6bfae9a8dccc2dd9e1afc309d995d
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-04 00:10:01 +0300

    update getUserByToken

--- app/src/main/java/com/example/guapstudios/data/retrofitService/AuthorizationRetrofitService.kt
+++ app/src/main/java/com/example/guapstudios/data/retrofitService/AuthorizationRetrofitService.kt
@@ -6,7 +6,9 @@ import com.example.guapstudios.data.modelForJSON.TokenModel
 import com.example.guapstudios.data.emptities.User
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface AuthorizationRetrofitService {
     @POST("login")
@@ -15,6 +17,6 @@ interface AuthorizationRetrofitService {
     @POST("register")
     fun register(@Body registerReciveModel: RegisterReciveModel): Call<TokenModel>
 
-    @POST("token")
-    fun getUserByToken(@Body tokenModel: TokenModel): Call<User>
+    @GET("performToken")
+    fun getUserByToken(@Query("token") token: String): Call<User>
 }
