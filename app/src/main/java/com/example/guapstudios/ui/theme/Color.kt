f8952dd7ceb2e79ff51b6a404f850e4c2c9299d3
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-07 21:07:01 +0300

    Изменение цветовой палитры

--- app/src/main/java/com/example/guapstudios/ui/theme/Color.kt
+++ app/src/main/java/com/example/guapstudios/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.guapstudios.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Magenta = Color(0xFF7879F1)
-val Magenta2 = Color(0xFFA5A6F6)
+val Magenta = Color(0xFF7C509B)
+val Magenta2 = Color(0xFFE95785 )
 val Red = Color(0xffFFAFAF)
-val Yellow = Color(0xFFFFE4AF)
+val Yellow = Color(0xFFFFD09D)
 val Green = Color(0xffC1FFE1)
 val Blue = Color(0xFF6FB6DE)
-val Gray = Color(0xFFFAFAFA)
+val Gray = Color(0xFFF2F2F2)
