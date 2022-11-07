b47c6f770b6e21fa00cd493b7b95ffc59acb5b06
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-07 13:42:35 +0300

    create ProjectReciveModel for update and add

--- app/src/main/java/com/example/guapstudios/data/modelForJSON/ProjectReciveModel.kt
+++ app/src/main/java/com/example/guapstudios/data/modelForJSON/ProjectReciveModel.kt
@@ -1,5 +1,7 @@
 package com.example.guapstudios.data.modelForJSON
 
+import kotlinx.serialization.Serializable
+
 @kotlinx.serialization.Serializable
 data class ListStringReceiveModel(
     val ides: Array<String>
@@ -10,3 +12,23 @@ data class ListResponceModel<T>(
     val tasks: List<T>,
     val failTasksIndex: List<String>
 )
+
+@Serializable
+data class ProjectReceiveModel(
+    val studio: String,
+    val adminId: String,
+    val title: String,
+    val description: String,
+)
+
+@Serializable
+data class ProjectUpdateReceiveModel(
+    val id: String,
+    val adminId: String? = null,
+    val title: String? = null,
+    val tasks: Array<String>? = null,
+    val description: String? = null,
+    val events: Array<String>? = null,
+    val users: Array<String>? = null,
+)
+
