1ce50efe4528753d13f877231b691833cf2753a7
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-07 13:49:42 +0300

    Добавление функций add и update в интерфейс retrofit 2

--- app/src/main/java/com/example/guapstudios/data/retrofitService/ProjectRetrofitService.kt
+++ app/src/main/java/com/example/guapstudios/data/retrofitService/ProjectRetrofitService.kt
@@ -3,6 +3,8 @@ package com.example.guapstudios.data.retrofitService
 import com.example.guapstudios.data.emptities.Project
 import com.example.guapstudios.data.modelForJSON.ListResponceModel
 import com.example.guapstudios.data.modelForJSON.ListStringReceiveModel
+import com.example.guapstudios.data.modelForJSON.ProjectReceiveModel
+import com.example.guapstudios.data.modelForJSON.ProjectUpdateReceiveModel
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -12,4 +14,10 @@ import retrofit2.http.Query
 interface ProjectRetrofitService {
     @POST("/project/get")
     fun getProjects(@Body projects: ListStringReceiveModel): Call<ListResponceModel<Project>>
+
+    @POST("/project/add")
+    fun addProject(@Body project: ProjectReceiveModel): Call<String>
+
+    @POST("/project/update")
+    fun updateProject(@Body projectUpdateReceiveModel: ProjectUpdateReceiveModel): Call<String>
 }
