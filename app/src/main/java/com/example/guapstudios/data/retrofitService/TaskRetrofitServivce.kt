f6fe9cbd32ec168736277c8bc060c23096c4a930
4118BarbolinS.P <skat.apps@gmail.com>
2022-12-14 02:05:16 +0300

    Добавление возвращаемого типа для функции getTasks

--- app/src/main/java/com/example/guapstudios/data/retrofitService/TaskRetrofitServivce.kt
+++ app/src/main/java/com/example/guapstudios/data/retrofitService/TaskRetrofitServivce.kt
@@ -5,10 +5,10 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-interface TaskRetrofitSerivce {
+interface TaskRetrofitServivce {
 
     @POST("project/tasks")
-    fun getTasks(@Body tasks: ListStringReceiveModel)
+    fun getTasks(@Body tasks: ListStringReceiveModel): Call<ListResponceModel<TaskDTO>>
 
     @POST("/project/tasks/add")
     fun addTask(@Body task: CreateTaskReceiveModel): Call<StringResponceModel>
