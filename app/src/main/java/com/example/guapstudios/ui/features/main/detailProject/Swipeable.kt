abff803eb08e6795d5efc10e8d36dc692cf0649f
4118BarbolinS.P <skat.apps@gmail.com>
2022-12-15 18:34:51 +0300

    Добавление возврата карточки после свайпа

--- app/src/main/java/com/example/guapstudios/ui/features/main/detailProject/Swipeable.kt
+++ app/src/main/java/com/example/guapstudios/ui/features/main/detailProject/Swipeable.kt
@@ -1,43 +1,63 @@
 package com.example.guapstudios.ui.features.main.detailProject
 
-import android.graphics.drawable.GradientDrawable
-import android.util.Log
+import android.graphics.Color
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import com.example.guapstudios.ui.theme.Gray
+import androidx.compose.ui.res.painterResource
 import com.example.guapstudios.ui.theme.Green
 import com.example.guapstudios.ui.theme.Red
-import kotlin.math.roundToInt
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SwipeContainer(
     modifier: Modifier,
-    backgroundLeftContent: @Composable () -> Unit,
-    backgroundRightContent: @Composable () -> Unit,
+    dismissState: DismissState,
+    leftIcon: Int,
+    rightIcon: Int,
+    rightSwipeAction: () -> Unit,
+    leftSwipeAction: () -> Unit,
     content: @Composable () -> Unit,
 ) {
-    val dismissState = rememberDismissState(initialValue = DismissValue.Default)
+
+    val r = remember { mutableStateOf(false) }
+
+    rememberCoroutineScope().launch {
+        if (r.value) {
+            dismissState.reset()
+            r.value = false
+        }
+    }
+
+    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+        r.value = true
+        rightSwipeAction()
+    }
+
+    if (dismissState.isDismissed(DismissDirection.StartToEnd) && r.value.not()) {
+        r.value = true
+        leftSwipeAction()
+    }
 
     SwipeToDismiss(modifier = modifier, state = dismissState, background = {
         val direction = dismissState.dismissDirection
 
         val color = if (direction == DismissDirection.StartToEnd) Green else Red
 
-        Box(Modifier.background(color).fillMaxSize()) {
-            if (direction == DismissDirection.StartToEnd) {
-                backgroundLeftContent()
-            } else {
-                backgroundRightContent()
-            }
+        val arrangement =
+            if (direction == DismissDirection.StartToEnd) Arrangement.Start else Arrangement.End
+
+        val icon = if (direction == DismissDirection.StartToEnd) leftIcon else rightIcon
+
+        Row(
+            Modifier
+                .background(color)
+                .fillMaxSize(), horizontalArrangement = arrangement
+        ) {
+            Icon(painter = painterResource(id = icon), contentDescription = "action")
         }
     }) {
         content()
