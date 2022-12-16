f4027d9bf51d8ee70d9040fd6fb10503b98e285a
4118BarbolinS.P <skat.apps@gmail.com>
2022-12-16 22:34:31 +0300

    Реализация вывода детальной информации

--- app/src/main/java/com/example/guapstudios/ui/features/techTask/TechTaskDetail.kt
+++ app/src/main/java/com/example/guapstudios/ui/features/techTask/TechTaskDetail.kt
@@ -1,13 +1,27 @@
 package com.example.guapstudios.ui.features.techTask
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.guapstudios.data.emptities.TechTask
+import com.example.guapstudios.ui.navigation.TechTaskScreens
+import com.example.guapstudios.ui.theme.Gray
+import com.example.guapstudios.ui.theme.Magenta
 import com.example.guapstudios.viewModel.TechTaskViewModel
 import com.skydoves.landscapist.glide.GlideImage
 
@@ -15,12 +29,86 @@ import com.skydoves.landscapist.glide.GlideImage
 fun TechTaskDetail(techTask: TechTask) {
     val viewModel: TechTaskViewModel = viewModel()
 
-    Column {
-        GlideImage(imageModel = "https://picsum.photos/1800", modifier = Modifier.fillMaxWidth())
+    Box(
+        Modifier.background(
+            brush = Brush.linearGradient(
+                colors = listOf(
+                    techTask.getColor(),
+                    Color.White
+                )
+            )
+        )
+    ) {
 
-        Card(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.padding(start = 16.dp, top = 100.dp),
+            text = techTask.title,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontSize = 64.sp
+        )
+
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 200.dp),
+            shape = RoundedCornerShape(topEnd = 64.dp),
+        ) {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+            ) {
+                TextWithValueAndKey(
+                    key = "Время: ",
+                    text = techTask.getStringDate().substring(startIndex = 0, endIndex = 5),
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp)
+                )
+
+                TextWithValueAndKey(
+                    key = "Дата: ",
+                    text = techTask.getStringDate().substring(startIndex = 5)
+                )
+
+                TextWithValueAndKey(
+                    key = "Заказчик: ",
+                    text = techTask.costumer
+                )
+
+                TextWithValueAndKey(
+                    key = "Место: ",
+                    text = techTask.place
+                )
+
+                Text(text = techTask.description, fontSize = 20.sp, modifier = Modifier.padding(16.dp))
+
+            }
 
         }
     }
 
+}
+
+@Composable
+fun TextWithValueAndKey(
+    key: String,
+    text: String,
+    modifier: Modifier = Modifier.padding(start = 16.dp, top = 8.dp)
+) {
+    Text(
+        buildAnnotatedString {
+            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                append(key)
+            }
+
+            append(text)
+        },
+        modifier = modifier,
+        fontSize = 20.sp
+    )
+
+    Spacer(modifier = Modifier
+        .height(3.dp)
+        .fillMaxWidth()
+        .background(Gray))
 }
