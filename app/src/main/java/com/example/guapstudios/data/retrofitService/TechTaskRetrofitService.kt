c0e8109e4584c9d6c75af32ae7c5d916203a7202
4118BarbolinS.P <skat.apps@gmail.com>
2022-12-16 23:33:07 +0300

    Создание запросов обновления полей isTake и Executor

--- app/src/main/java/com/example/guapstudios/data/retrofitService/TechTaskRetrofitService.kt
+++ app/src/main/java/com/example/guapstudios/data/retrofitService/TechTaskRetrofitService.kt
@@ -5,11 +5,19 @@ import com.example.guapstudios.data.modelForJSON.ListResponceModel
 import com.example.guapstudios.data.modelForJSON.ListStringReceiveModel
 import com.example.guapstudios.data.modelForJSON.ProjectDeleteReceiveModel
 import com.example.guapstudios.data.modelForJSON.StringResponceModel
+import com.skat.database.tech_task.TechTaskUpdateExecutor
+import com.skat.database.tech_task.TechTaskUpdateIsTake
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface TechTaskRetrofitService {
+    @POST("/tech_task/update/take")
+    fun updateIsTakeTechTask(@Body model: TechTaskUpdateIsTake): Call<StringResponceModel>
+
+    @POST("/tech_task/update/executor")
+    fun updateExecutorTechTask(@Body model: TechTaskUpdateExecutor): Call<StringResponceModel>
+
     @POST("/tech_task/add")
     fun addTechTask(@Body techTask: TechTask): Call<StringResponceModel>
 
