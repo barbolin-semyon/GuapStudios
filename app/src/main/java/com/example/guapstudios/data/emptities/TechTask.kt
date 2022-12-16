9898630898b95c5df956d5d14a00472b5eab50a3
4118BarbolinS.P <skat.apps@gmail.com>
2022-12-16 21:40:00 +0300

    Добавление parcelize

--- app/src/main/java/com/example/guapstudios/data/emptities/TechTask.kt
+++ app/src/main/java/com/example/guapstudios/data/emptities/TechTask.kt
@@ -1,8 +1,11 @@
 package com.example.guapstudios.data.emptities
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.text.SimpleDateFormat
 import java.util.*
 
+@Parcelize
 @kotlinx.serialization.Serializable
 data class TechTask(
     val costumer: String,
@@ -14,7 +17,7 @@ data class TechTask(
     val isTake: Boolean,
     val place: String,
     private val date: String
-) {
+) : Parcelable {
     fun getDate(): Date? {
         val format = "HH:mm dd.MM.yyyy"
         return SimpleDateFormat(format).parse(date)
